c87baf800e109d06a113728a2552da8860ed0bee
Davi Marcilio <davimarcilio.js@gmail.com>
2022-09-02 16:51:49 -0300

    aula 1

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
 const app = express();
-const PORT = 3000
+const PORT = 3000;
+
+
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
 
+    res.render('user');
 
 })
 
