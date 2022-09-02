62b61c558c7038f8db741ac859cd0915f1ea5f98
Davi Marcilio <davimarcilio.js@gmail.com>
2022-09-02 17:00:49 -0300

    aula3

--- index.js
+++ index.js
@@ -5,14 +5,26 @@ const app = express();
 const PORT = 3000;
 
 
-
+let users = [{
+    id: 0,
+    name: 'Davi',
+    phone: '(11)999999',
+}, {
+    id: 1,
+    name: 'Davi2',
+    phone: '(112)22999999',
+}, {
+    id: 2,
+    name: 'Davi3',
+    phone: '(113)333999999',
+}]
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
 
-    res.render('user', { name: 'Davi', phone: '(48) 99999999' });
+    res.render('user', { users });
 
 })
 
